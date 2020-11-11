309edfffb3216f3b2ea4e13a7faeb0fcfa0dbecb
marcos-globalinternet <marcos.prieto@globalinter.net>
2020-11-11 16:13:36 +0100

    feat: todolist item component

---
+++ src/components/presentational/TodoList/TodoListItem/TodoListItem.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import styles from './TodoListItem.module.scss';
+
+type StateProps = {
+  taskId: string;
+  taskName: string;
+  taskDone: boolean;
+  taskPriorityColor: string;
+}
+
+type DispatchProps = {
+  onTaskChangeStatus: (taskId: string, taskDone: boolean) => void;
+}
+
+type Props = StateProps & DispatchProps;
+
+export const TodoListItem: React.FC<Props> = (props: Props) => {
+  const [taskDone, setTaskDone] = useState<boolean>(false);
+
+  useEffect(() =>  {
+    setTaskDone(props.taskDone);
+  }, [props.taskDone]);
+
+  const checkboxChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskDone(event.target.checked);
+    props.onTaskChangeStatus(props.taskId, event.target.checked);
+  }
+
+	return (
+    <div style={{borderLeftColor: props.taskPriorityColor}} className={styles.wrapper}>
+      <span className={`${taskDone ? styles.crossed : ''}`}>{props.taskName}</span>
+      <input className={styles.checkbox} type='checkbox' checked={taskDone} onChange={checkboxChangeHandler} />
+    </div>
+  );
+}
