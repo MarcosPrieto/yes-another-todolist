cd7b7a36b20d4b3b0949bbab9d318bbbcb86f6ad
Marcos Prieto <marcosprietosi@gmail.com>
2023-03-02 17:07:58 +0100

    feat: remove toastify

--- client/src/stories/Organisms/Create.stories.tsx
+++ client/src/stories/Organisms/Create.stories.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { Create } from '../../components/containers/Create/Create';
-import { ToastContainer } from 'react-toastify';
 
 export default {
   title: 'Organisms/Create',
@@ -12,7 +11,6 @@ type Props = React.ComponentProps<typeof Create>;
 
 const Template: Story<Props> = (args) =>
   <Fragment>
-    <ToastContainer />
     <Create {...args} />
   </Fragment>;
 
