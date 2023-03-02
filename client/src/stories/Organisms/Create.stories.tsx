265a664e860a270527aab921f1976dadeaba93d3
Marcos <36364231+MarcosPrieto@users.noreply.github.com>
2023-03-02 18:51:45 +0100

    Feature/configure storybook (#24)
    * feat: modify storybook to fix issues and adapt to Vite
    
    * feat: remove wrong include in tsconfig

--- client/src/stories/Organisms/Create.stories.tsx
+++ client/src/stories/Organisms/Create.stories.tsx
@@ -1,17 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { MemoryRouter } from 'react-router-dom';
+
+// Components
 import { Create } from '../../components/containers/Create/Create';
 
 export default {
   title: 'Organisms/Create',
   component: Create,
+  decorators: [
+    (Story, context) => (
+      <MemoryRouter initialEntries={['/', '/create']}>
+        <Story {...context} />
+      </MemoryRouter>
+    ),
+  ]
 } as Meta;
 
 type Props = React.ComponentProps<typeof Create>;
 
-const Template: Story<Props> = (args) =>
-  <Fragment>
-    <Create {...args} />
-  </Fragment>;
+const Template: Story<Props> = (args) => <Create {...args} />;
 
 export const CreateDefault = Template.bind({});
