370a9ffda7273be4051376cc7d00e07e3944baf1
Marcos Prieto <marcosprietosi@gmail.com>
2023-03-02 18:49:14 +0100

    feat: modify storybook to fix issues and adapt to Vite

--- client/src/stories/Organisms/TodoList.stories.tsx
+++ client/src/stories/Organisms/TodoList.stories.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { MemoryRouter } from 'react-router-dom';
+
+// Components
 import { TodoList } from '../../components/containers/TodoList/TodoList';
 
 export default {
   title: 'Organisms/TodoList',
   component: TodoList,
+  decorators: [
+    (Story, context) => (
+      <MemoryRouter initialEntries={['/', '/create']}>
+        <Story {...context} />
+      </MemoryRouter>
+    )
+  ]
 } as Meta;
 
 type Props = React.ComponentProps<typeof TodoList>;
