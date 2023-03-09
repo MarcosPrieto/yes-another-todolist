db919d3a60dd2d4a3d5d7ec1beb4f58b32936050
Marcos <36364231+MarcosPrieto@users.noreply.github.com>
2023-03-09 23:05:31 +0100

    fix: problem with empty tasklist displays NaN, and fix progress font color (#31)

--- client/src/components/presentational/ProgressBar/ProgressBar.tsx
+++ client/src/components/presentational/ProgressBar/ProgressBar.tsx
@@ -3,11 +3,16 @@ import { useSpring, animated } from 'react-spring';
 // Styles
 import styles from './ProgressBar.module.scss';
 
+// Components
+import { useTheme } from '../../hoc/ThemeProvider/ThemeProvider';
+
 type Props = {
   progress: number;
 }  & React.HTMLAttributes<HTMLDivElement>;
 
 const ProgressBar: React.FC<Props> = ({ progress, ...otherProps }: Props) => {
+  const { theme } = useTheme();
+
   const number = useSpring({
     from: { number: 0 },
     number: progress,
@@ -24,7 +29,7 @@ const ProgressBar: React.FC<Props> = ({ progress, ...otherProps }: Props) => {
       aria-valuemax={100}>
       <div className={styles.progressBar__progress} style={{ width: `${progress}%` }}>
         <animated.span
-          className={styles.progressBar__text} style={{ marginRight: progress < 5 ? '-2.5rem' : '0' }}
+          className={`${progress < 5 ? 'themeText' : ''} ${styles.progressBar__text}`} style={{ marginRight: progress < 5 ? '-2.5rem' : '0'}}
         >
           {number.number.to((n: number) => `${n.toFixed(0)} %`)}
         </animated.span>
