4e1523e4ac794f0712f02bab836a47e8ca5db686
Marcos Prieto <marcosprietosi@gmail.com>
2023-03-21 13:24:05 +0100

    feat: create and modify zustand stores

---
+++ client/src/store/configuration.store.ts
@@ -0,0 +1,109 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import { toast } from 'react-hot-toast';
+
+// Services
+import { ping } from '../services/general.service';
+
+// Types
+import { CONNECTION_STATE, STORE_MODE } from '../typings/common.types';
+
+export type State = {
+  isLoginVisible: boolean;
+  storeMode: STORE_MODE;
+  connectionState: CONNECTION_STATE;
+  connectionErrors: number;
+  reconnectToServerListeners: (() => void)[];
+}
+
+type Actions = {
+  getStoreMode: () => STORE_MODE;
+  setStoreMode: (storeMode: STORE_MODE) => void;
+  increaseConnectionErrors: () => void;
+  tryToReconnectToServer: () => void;
+  setConnectionState: (connectionState: CONNECTION_STATE) => void;
+  reconnectedToServer: (callback: () => void) => void;
+}
+
+export type ConfigurationState = State & Actions;
+
+export const useConfigurationStore = create<ConfigurationState>()(
+  // @ts-ignore
+  devtools((set, get) => ({
+    storeMode: 'offline',
+    connectionState: 'connected',
+    connectionErrors: 0,
+    reconnectToServerListeners: [],
+
+    getStoreMode: () => {
+      return get().storeMode;
+    },
+
+    setStoreMode: (storeMode: STORE_MODE) => {
+      if (get().storeMode === 'offline' && storeMode === 'online') {
+        get().reconnectToServerListeners.forEach((cb) => cb);
+      }
+      set({
+        storeMode
+      });
+    },
+
+    tryToReconnectToServer: () => {
+      let retries = 20;
+
+      const checkIsConnectedToServer = async () => {
+        const isOnline = await ping();
+        if (isOnline) {
+
+          get().setConnectionState('connected');
+
+          toast.success('Connection to the server has been restored');
+          get().reconnectToServerListeners.forEach((cb) => cb);
+          clearInterval(isOnlineInterval);
+        } else {
+          retries--;
+          if (retries === 0) {
+            clearInterval(isOnlineInterval);
+          }
+        }
+      };
+
+      const isOnlineInterval = setInterval(checkIsConnectedToServer, 120000);
+    },
+
+    increaseConnectionErrors: () => {
+      set({ connectionErrors: get().connectionErrors + 1 });
+
+      if (get().connectionErrors > 5) {
+        set({ connectionErrors: 0 });
+        get().setConnectionState('serverError');
+      }
+    },
+
+    setConnectionState: (connectionState: CONNECTION_STATE) => {
+      if (get().connectionState === 'connected' && connectionState === 'serverError' && get().storeMode === 'online') {
+        toast.error('There is a problem with the connection to the server. You are working in offline mode. Your changes will be synchronized when the connection is restored');
+        set({ storeMode: 'error' });
+        get().tryToReconnectToServer();
+      }
+      if (get().connectionState === 'connected' && connectionState === 'disconnected' && get().storeMode === 'online') {
+        set({ storeMode: 'error' });
+        toast.error('There is no internet connection. You are working in offline mode. Your changes will be synchronized when the connection is restored');
+      }
+      if (get().connectionState !== 'connected' && connectionState === 'connected' && get().storeMode === 'error') {
+        set({ storeMode: 'online' });
+        toast.success('Internet connection has been restored');
+        get().reconnectToServerListeners.forEach((cb) => cb);
+      }
+      set({ connectionState });
+    },
+
+    reconnectedToServer: (callback: () => void) => {
+      set((state) => ({ reconnectToServerListeners: [...new Set([...state.reconnectToServerListeners, callback])]}));
+
+      return () => {
+        set((state) => ({ reconnectToServerListeners: state.reconnectToServerListeners.filter((listener) => listener !== callback) }));
+      };
+    }
+  }))
+);
--- client/src/store/task.store.ts
+++ client/src/store/task.store.ts
@@ -1,52 +1,145 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { createJSONStorage, devtools, persist } from 'zustand/middleware';
+import { toast } from 'react-hot-toast';
 
 // Models
 import { Task } from '../models/task.model';
 
-// Services
-import { changeStatus, createTask, deleteTask, fetchTasks, updateTask } from '../services/tasks.service';
+//Store
+import { useConfigurationStore } from './configuration.store';
 
-export type State = {
+// Services
+import { changeStatus, createTask, deleteTask, fetchUserTasks, updateTask, syncTasks } from '../services/tasks.service';
+import { useAuthStore } from './auth.store';
+
+const getStoreMode = () => useConfigurationStore.getState().getStoreMode();
+const getUser = () => useAuthStore.getState().getUser();
+
+
+type State = {
   tasks: Task[];
+  hasHydrated: boolean;
 }
 
 type Actions = {
   setTasks: (tasks: Task[]) => void;
   clearTasks: () => void;
   fetchTasks: () => Promise<void>;
+  getTasks: () => Task[];
+  getCompletedTasks: () => Task[];
+  getPendingTasks: () => Task[];
   addTask: (task: Task) => Promise<void>;
   changeTaskStatus: (taskId: string, done: boolean) => Promise<void>;
   updateTask: (task: Task) => Promise<void>;
   deleteTask: (taskId: string) => Promise<void>;
+  syncOfflineTasks: () => void;
 }
 
 export type TaskState = State & Actions;
 
 export const useTaskStore = create<TaskState>()(
-  devtools((set) => ({
+  // @ts-ignore
+  persist(devtools((set, get) => ({
     tasks: [],
+    hasHydrated: false,
 
     setTasks: (tasks: Task[]) => set({ tasks }),
 
     clearTasks: () => set({ tasks: [] }),
 
+    getTasks: () => get().tasks
+      .filter((task) => !task.deleted)
+      .sort((taskA, taskB) => (taskA.priority - taskB.priority)),
+
+    getCompletedTasks: () => get().getTasks().filter((task) => task.done),
+
+    getPendingTasks: () => get().getTasks().filter((task) => !task.done),
+
     fetchTasks: async () => {
-      const response = await fetchTasks();
-      set({ tasks: response });
+      const user = getUser();
+      if (getStoreMode() === 'offline' || !user) {
+        return;
+      }
+
+      const response = await fetchUserTasks(user.id);
+      if (!response) {
+        return;
+      }
+
+      const { existingTasksFromResponse, newTaskFromResponse } = 
+        response.reduce((acc: {existingTasksFromResponse: Task[], newTaskFromResponse: Task[]}, task) => {
+          if (get().tasks.find((t) => t.id === task.id)) {
+            acc.existingTasksFromResponse.push(task);
+          } else {
+            acc.newTaskFromResponse.push(task);
+          }
+          return acc;
+        }, { existingTasksFromResponse: [], newTaskFromResponse: [] });
+
+      const mergedExistingTasks = get().tasks.map((task) => {
+        const matchingTaskFromResponse = existingTasksFromResponse.find((t) => t.id === task.id);
+        if (matchingTaskFromResponse) {
+          return {...matchingTaskFromResponse, ...task};
+        }
+        return task;
+      });
+
+      const finalTasks = mergedExistingTasks.concat(newTaskFromResponse);
+
+      set({tasks: finalTasks});
     },
 
     addTask: async (task: Task) => {
-      await createTask(task);
-      set((state) => ({ tasks: [...state.tasks, task] }));
+      const existsTaskWithSameName = () =>
+        get()
+          .getTasks()
+          .some((t) => 
+            t.displayName === task.displayName 
+            && (task.categoryId ? t.categoryId === task.categoryId : true)
+          );
+
+      if (existsTaskWithSameName()) {
+        toast.error(`Task with name "${task.displayName}" already exists${task.categoryId ? ` in category "${task.categoryId}"` : ''}`);
+        return;
+      }
+
+      const createdTask: Task = {
+        ...task,
+        createdAt: new Date().toISOString(),
+        userId: getUser()?.id,
+        deleted: false,
+        syncStatus: 'unsynced',
+      };
+
+      if (getStoreMode() === 'online') {
+        await createTask(createdTask).then(() => {
+          createdTask.syncStatus = 'synced';
+        }).catch(() => {
+          createdTask.syncStatus = 'error';
+        });
+      }
+      set((state) => ({ tasks: [...state.tasks, createdTask] }));
     },
 
     changeTaskStatus: async (taskId: string, done: boolean) => {
-      await changeStatus(taskId, done);
+      const updatedTask: Task = {
+        ...get().tasks.find((task) => task.id === taskId) as Task,
+        done,
+        updatedAt: new Date().toISOString(),
+        syncStatus: 'unsynced'
+      };
+
+      if (getStoreMode() === 'online') {
+        await changeStatus(taskId, done).then(() => {
+          updatedTask.syncStatus = 'synced';
+        }).catch(() => {
+          updatedTask.syncStatus = 'error';
+        });
+      }
       set((state) => ({
         tasks: state.tasks.map((task) => {
           if (task.id === taskId) {
-            return { ...task, done };
+            return updatedTask;
           }
           return task;
         }),
@@ -54,11 +147,40 @@ export const useTaskStore = create<TaskState>()(
     },
 
     updateTask: async (task: Task) => {
-      await updateTask(task);
+      const existsTaskWithSameName = () =>
+        get()
+          .getTasks()
+          .some((t) => 
+            t.id !== task.id
+            && t.displayName === task.displayName 
+            && (task.categoryId ? t.categoryId === task.categoryId : true)
+          );
+
+      if (existsTaskWithSameName()) {
+        toast.error(`Task with name "${task.displayName}" already exists${task.categoryId ? ` in category "${task.categoryId}"` : ''}`);
+        return;
+      }
+
+      const existingTask = get().getTasks().find((t) => t.id === task.id);
+
+      const updatedTask: Task = {
+        ...existingTask,
+        ...task,
+        updatedAt: new Date().toISOString(),
+        syncStatus: 'unsynced'
+      };
+
+      if (getStoreMode() === 'online') {
+        await updateTask(task).then(() => {
+          updatedTask.syncStatus = 'synced';
+        }).catch(() => {
+          updatedTask.syncStatus = 'error';
+        });
+      }
       set((state) => ({
         tasks: state.tasks.map((t) => {
           if (t.id === task.id) {
-            return task;
+            return updatedTask;
           }
           return t;
         }),
@@ -66,12 +188,69 @@ export const useTaskStore = create<TaskState>()(
     },
 
     deleteTask: async (taskId: string) => {
-      await deleteTask(taskId);
-      set((state) => ({ tasks: state.tasks.filter((task) => task.id !== taskId) }));
+      if (getStoreMode() === 'online') {
+        await deleteTask(taskId);
+      } else {
+        set((state) => ({
+          tasks: state.tasks.map((task) => {
+            if (task.id === taskId) {
+              return { ...task, deleted: true, syncStatus: 'unsynced' };
+            }
+            return task;
+          }),
+        }));
+      }
     },
-  })),
+
+    syncOfflineTasks: () => {
+      const user = getUser();
+      if (getStoreMode() !== 'online' || !user) {
+        return;
+      }
+
+      const tasksToSync = get().tasks
+        .filter((task) => task.syncStatus !== 'synced')
+        .map((task) => (
+          {...task,
+            userId: user.id,
+          })
+        );
+
+      const syncingTasks = syncTasks(user.id, tasksToSync).then(() => {
+        set((state) => {
+          const updatedTasks = [...state.tasks].map((task) => {
+            if (task.syncStatus !== 'synced') {
+              return { ...task, syncStatus: 'synced' };
+            }
+          }).filter((task) => task && !task.deleted) as Task[];
+
+          return { tasks: updatedTasks };
+        });
+      }).catch(() => {
+        set((state) => ({
+          tasks: state.tasks.map((task) => {
+            if (task.syncStatus === 'unsynced') {
+              return { ...task, syncStatus: 'error' };
+            }
+            return task;
+          }),
+        }));
+      });
+
+      toast.promise(
+        syncingTasks,
+        {
+          loading: 'Syncing tasks...',
+          success: 'Tasks have been synced',
+          error: 'Error while syncing tasks',
+        },
+      );
+    }
+  })), {
+    name: 'configuration-storage',
+    storage: createJSONStorage(() => getStoreMode() === 'online' ? sessionStorage : localStorage)
+  })
 );
 
 // selectors
-
 export const getPercentageCompletedTasks = (state: State) => ((state.tasks?.filter(task => task.done).length / state.tasks?.length * 100) || 0);
