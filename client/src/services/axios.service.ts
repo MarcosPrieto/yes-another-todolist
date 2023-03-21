deec4631b26c221371680180f3de19b3a5d02e19
Marcos Prieto <marcosprietosi@gmail.com>
2023-03-21 13:15:21 +0100

    feat: add bearer auth to Axios, and retry only on Server errors

--- client/src/services/axios.service.ts
+++ client/src/services/axios.service.ts
@@ -1,14 +1,21 @@
 import axios, { AxiosInstance } from 'axios';
 import axiosRetry from 'axios-retry';
 
+// Store
+import { getTokenNonReactComponent } from '../store/auth.store';
 
 export const getAxiosApiInstance = (endpoint: string): AxiosInstance => {
   const client = axios.create({
     baseURL: endpoint,
     headers: {
       'Content-Type': 'application/json'
-    },
+    }
   });
+
+  const token = getTokenNonReactComponent();
+  if (token) {
+    client.defaults.headers.common.Authorization = `Bearer ${token}`;
+  }
 
   configureRetry(client);
 
@@ -16,12 +23,12 @@ export const getAxiosApiInstance = (endpoint: string): AxiosInstance => {
 };
 
 const configureRetry = (client: AxiosInstance) => {
-  const retries = Number(import.meta.env.VITE_API_AXIOS_RETRIES || 3);
+  const retries = Number(import.meta.env.VITE_API_AXIOS_RETRIES || 5);
 
   axiosRetry(client, {
     retries, retryDelay: axiosRetry.exponentialDelay, retryCondition: (error) => {
       const errorCode = error.response?.status || 200;
-      if (errorCode >= 400) {
+      if (errorCode >= 500) {
         return true;
       }
       return false;
