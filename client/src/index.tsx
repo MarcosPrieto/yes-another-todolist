2830db7e6a9bb3e5a2423d1ccdcfdf64e0ae6342
Marcos Prieto <marcosprietosi@gmail.com>
2023-03-21 13:14:20 +0100

    refactor: modify index for more efficient loading

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { worker } from './mocks/browser';
-
 // Style
 import './index.scss';
-
-// Services
-import { windowReferenceService } from './services/system/windowsReference.service';
 
 const FAKE_API = import.meta.env.VITE_APP_FAKE_API;
 
@@ -15,19 +10,17 @@ const FAKE_API = import.meta.env.VITE_APP_FAKE_API;
 import App from './App';
 import ThemeProvider from './components/hoc/ThemeProvider/ThemeProvider';
 
-if (import.meta.env.DEV) {
-  if (!windowReferenceService().Cypress) {
-    // runs the in-memory testing API
-    if (import.meta.env.DEV && FAKE_API === 'true') {
-      worker.start();
-    }
+const prepare = async (): Promise<void> => {
+  if (import.meta.env.DEV && FAKE_API === 'true') {
+    const { worker } = await import('./mocks/browser');
+    worker.start();
   }
-}
+};
 
-createRoot(document.getElementById("root") as HTMLElement).render(
+prepare().then(() => createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
   </React.StrictMode>
-);
+));
