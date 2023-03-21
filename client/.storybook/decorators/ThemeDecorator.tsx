a42f811f7fa3762a35ab763b62bce9d77aa9a237
Marcos Prieto <marcosprietosi@gmail.com>
2023-03-21 13:07:17 +0100

    feat: create a Storybook theme decorator without styles

--- client/.storybook/decorators/ThemeDecorator.tsx
+++ client/.storybook/decorators/ThemeDecorator.tsx
@@ -1,31 +1,13 @@
-import { useState } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 // Styles
 import '../../src/App.scss';
 
-// Context
-import { ThemeContext } from '../../src/components/hoc/ThemeProvider/ThemeProvider';
-
-// Constants
-import { THEME } from '../../src/constants/theme.constants';
+// Decorators
+import ThemeDecoratorNoStyle from './ThemeDecoratorNoStyle';
 
 const ThemeDecorator = (Story: Story, context: Meta) => {
-
-  const [theme, setTheme] = useState<THEME>('light');
-
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
-
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <button onClick={toggleTheme} style={{marginBottom: '20px'}}>Theme toggle</button>
-      <div style={{padding: '20px'}} className={`${theme}-theme app`}>
-        <Story {...context} />
-      </div>
-    </ThemeContext.Provider>
-  );
+  return ThemeDecoratorNoStyle(Story, context);
 }
 
 export default ThemeDecorator;
