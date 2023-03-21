948c030cd75f226e13168a205b9a00e361c29d84
Marcos Prieto <marcosprietosi@gmail.com>
2023-03-21 13:24:05 +0100

    feat: create and modify zustand stores

---
+++ client/src/store/auth.store.ts
@@ -0,0 +1,84 @@
+import { create } from 'zustand';
+import { devtools, persist, createJSONStorage } from 'zustand/middleware';
+
+// Models
+import { User, UserRequest, UserResponse } from '../models/user.model';
+
+// Services
+import { createUser, loginUser } from '../services/user.service';
+
+type State = {
+  user: User | null;
+  token: string | null;
+  isLoginVisible: boolean;
+}
+
+type Actions = {
+  createUser: (user: UserRequest) => Promise<void>;
+  login: (user: Omit<UserRequest, 'name'>) => Promise<void>;
+  logout: () => void;
+  getToken: () => string | null;
+  getUser: () => User | null;
+  isAuthorized: () => boolean;
+  setIsLoginVisible: (isLoginVisible: boolean) => void;
+}
+
+export type AuthState = State & Actions;
+
+export const useAuthStore = create<AuthState>()(
+  persist(devtools((set, get) => ({
+    user: null,
+    token: null,
+    isLoginVisible: false,
+
+    createUser: async (user: UserRequest) => {
+      const response = (await createUser(user)) as UserResponse;
+      if (!response) {
+        return;
+      }
+      set({ user: { id: response.id, name: response.name, email: response.email } });
+      set({ token: response.token });
+      set({ isLoginVisible: false });
+    },
+
+    login: async (user: Omit<UserRequest, 'name'>) => {
+      const response = (await loginUser(user)) as UserResponse;
+      if (!response) {
+        return;
+      }
+      set({ user: { id: response.id, name: response.name, email: response.email } });
+      set({ token: response.token });
+      set({ isLoginVisible: false });
+    },
+
+    logout: () => {
+      set({ user: null });
+      set({ token: null });
+      set({ isLoginVisible: true });
+    },
+    getToken: () => {
+      return get().token;
+    },
+    getUser: () => {
+      return get().user;
+    },
+    isAuthorized: () => {
+      return get().token !== null;
+    },
+
+    setIsLoginVisible: (isLoginVisible: boolean) => {
+      set({ isLoginVisible });
+    }
+  })), {
+    name: 'user-storage',
+    storage: createJSONStorage(() => sessionStorage)
+  })
+);
+
+export const getTokenNonReactComponent = () => {
+  const tokenZustardWrapper = useAuthStore.getState().getToken();
+
+  if (tokenZustardWrapper) {
+    return (tokenZustardWrapper as unknown as { value: string}).value;
+  }
+};
