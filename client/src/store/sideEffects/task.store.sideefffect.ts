3d7f7f4b644eb4615736bbc6c012566d0a5f0443
Marcos Prieto <marcosprietosi@gmail.com>
2023-03-28 19:15:38 +0200

    feat: zustand side effects

---
+++ client/src/store/sideEffects/task.store.sideefffect.ts
@@ -0,0 +1,37 @@
+import toast from 'react-hot-toast';
+import { StoreApi } from 'zustand';
+
+// Stores
+import { ConfigurationState } from '../configuration.store';
+import { TaskState } from '../task.store';
+
+/**
+ * Sync the offline tasks and fetch the tasks from the server
+ */
+export const syncAndFetchTasks = async (taskStore: StoreApi<TaskState>, configurationStore: StoreApi<ConfigurationState>, displayToast: boolean) => {
+  configurationStore.getState().setIsLoading(true);
+  
+  const functionArray = [
+    () => taskStore.getState().syncOfflineTasks(), 
+    () => taskStore.getState().fetchTasks()
+  ];
+
+  const secuentiallyExecute = async () => {
+    for (const fn of functionArray) {
+      await fn();
+    }
+  };
+
+  if (displayToast) {
+     toast.promise(
+      secuentiallyExecute(),
+      {
+        loading: 'Syncing tasks...',
+        success: 'Tasks have been synced',
+        error: 'Error while syncing tasks',
+      },
+    );
+  }
+
+  configurationStore.getState().setIsLoading(false);
+}
