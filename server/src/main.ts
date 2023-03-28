3c497a3e3d41e0b84923379948a7a204e5861df5
Marcos Prieto <marcosprietosi@gmail.com>
2023-03-28 19:34:16 +0200

    feat: update main

--- server/src/main.ts
+++ server/src/main.ts
@@ -18,7 +18,6 @@ import tokenRoutes from './routes/token.routes';
 // Controllers
 import { verifyCsrfToken, verifyToken } from './controllers/token.controller';
 
-
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
@@ -36,15 +35,14 @@ app.use(helmet());
 const COOKIE_PARSER_SECRET = process.env.COOKIE_PARSER_SECRET!;
 app.use(cookieParser(COOKIE_PARSER_SECRET));
 
+const isProduction = process.env.NODE_ENV! === 'production';
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 500, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
-app.use(limiter);
-
 
 const COOKIE_SESSION_SECRET = process.env.COOKIE_SESSION_SECRET!;
 app.use(cookieSesion({
@@ -54,24 +52,17 @@ app.use(cookieSesion({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
-
-const isProduction = process.env.NODE_ENV! === 'production';
-
 if (isProduction) {
-  //app.set('trust proxy', 1); // TODO: test this in production
+  app.use(limiter);
+  app.set('trust proxy', 1); // TODO: test this in production
 }
 
 app.use('/token', tokenRoutes);
-app.use('/auth', verifyCsrfToken, authRoutes);
+app.use('/auth', authRoutes);
 app.use('/generic', verifyCsrfToken, verifyToken, genericRoutes);
 app.use('/task', verifyCsrfToken, verifyToken, taskRoutes);
-
 
 mongoConnect(() => {
   console.log('listening to port 3001');
   app.listen(PORT);
 });
-
-
-
-
