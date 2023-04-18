3d1c73023a4bad80b601764a5ea91e098515ed5c
Marcos Prieto <marcosprietosi@gmail.com>
2023-04-18 22:26:35 +0200

    feat: sanitize backend fields

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { body, param } from 'express-validator';
 
 // Controllers
 import { login, signIn } from '../controllers/auth.controller';
@@ -6,9 +7,16 @@ import { login, signIn } from '../controllers/auth.controller';
 const router = express.Router();
 
 // /auth/login -> POST
-router.post('/login', login);
+router.post('/login',
+  body('email').isEmail().normalizeEmail(),
+  body('password').trim().escape(),
+login);
 
 // /auth/signin -> POST
-router.post('/signin', signIn);
+router.post('/signin',
+  body('email').isEmail().normalizeEmail(),
+  body('password').trim().escape(),
+  body('name').trim().escape(),
+signIn);
 
 export default router;
